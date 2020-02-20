528cb75ebc10a54ada3ea92f647999657f495859
nuxster <nuxster@borg.domain.local>
2020-02-20 16:35:37 +0300

    2020.02.20

--- LayoutEditor_utf8.py
+++ LayoutEditor_utf8.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding: UTF-8
 
 import time
@@ -38,30 +39,27 @@ class LEMainWindow(QtWidgets.QMainWindow):
         # self.template_data_model.setColumnCount(9)
         self.header_labels = ['Точка учета', 'Начало', 'Окончание', 'Статус', 'A+', 'A-', 'R+', 'R-']
         self.template_data_model.setHorizontalHeaderLabels(self.header_labels)
-        self.ui.templateDataTree.clicked.connect(lambda: self.treeview_clicked())
         self.ui.templateDataTree.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.ui.templateDataTree.setAlternatingRowColors(True)
         self.ui.templateDataTree.setAllColumnsShowFocus(True)
         self.ui.templateDataTree.setMinimumWidth(640)
         self.ui.templateDataTree.setMinimumHeight(480)
         self.ui.templateDataTree.setModel(self.template_data_model)
+        self.ui.templateDataTree.selectionModel().selectionChanged.connect(self.treeview_select_row)
         # Название вкладок
         self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.ui.tab_1), 'Статус')
         self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.ui.tab_2), 'Объём')
         # Общая область
         self.ui.label_selected_measuringpoint.setText('Присоединение')
-
         # Комбобоксы выбора интервала
         self.ui.startTime_label.setText('Начало:')
         self.ui.startPeriod_comboBox.setStyleSheet('combobox-popup: 0;')
         self.ui.endTime_label.setText('Окончание:')
         self.ui.endPeriod_comboBox.setStyleSheet('combobox-popup: 0;')
-
         # Вкладка 'Статус'
         # Выбор типа присоединения
         self.ui.measuringpointType_label.setText('Присоединения:')
         self.ui.comboBox_measuringpoint_type.addItems(('', 'Все некоммерч.', 'Все'))
-        
         # Инициализация интервалов
         self.populate_period(init=True)
         # Реинициализация времемени окончания после выбора времени начала
@@ -71,6 +69,16 @@ class LEMainWindow(QtWidgets.QMainWindow):
         self.ui.selectFlag_label.setText('Флаг:')
         self.ui.comboBox_select_flag.addItems(('0', '1'))
         # Вкладка 'Объем'
+        # Инициализация чекбоксов по каналам
+        self.ui.checkBox_save_a_plus.setText('Только получасовка')
+        self.ui.checkBox_save_a_plus.setChecked(True)
+        self.ui.checkBox_save_a_minus.setText('Только получасовка')
+        self.ui.checkBox_save_a_minus.setChecked(True)
+        self.ui.checkBox_save_r_plus.setText('Только получасовка')
+        self.ui.checkBox_save_r_plus.setChecked(True)
+        self.ui.checkBox_save_r_minus.setText('Только получасовка')
+        self.ui.checkBox_save_r_minus.setChecked(True)
+        #
         self.ui.label_a_plus.setText('Активная приём (A+)')
         self.ui.label_a_minus.setText('Активная отдача (A-)')
         self.ui.label_r_plus.setText('Реактивная приём (R+)')
@@ -104,11 +112,13 @@ class LEMainWindow(QtWidgets.QMainWindow):
         filemenu.addAction(self.exit_action)
 
 
-    def treeview_clicked(self):
+    def treeview_select_row(self):
+        '''
+        Действия при выборе строки в treeview.
+        '''
         indexes = self.ui.templateDataTree.selectedIndexes()
         measuringpoint = indexes[0].parent().data(QtCore.Qt.DisplayRole) or indexes[0].data(QtCore.Qt.DisplayRole)
         self.populate_comboBox_selected_measuringpoint(current_item=measuringpoint)
-
         try:
             # Вывод объемов на редактирование по каналам
             self.ui.lineEdit_a_plus.setText(indexes[5].data(QtCore.Qt.DisplayRole))
@@ -119,9 +129,6 @@ class LEMainWindow(QtWidgets.QMainWindow):
             self.populate_period(start_time=':'.join((indexes[1].data(QtCore.Qt.DisplayRole)[:2], indexes[1].data(QtCore.Qt.DisplayRole)[2:])))
         except IndexError:
             pass
-        
-        # self.ui.startPeriod_comboBox.currentIndexChanged.connect(lambda: self.populate_period(
-        #     start_time=self.ui.startPeriod_comboBox.currentText()))
 
 
     def populate_period(self, init=False, start_time="00:00"):
@@ -247,8 +254,6 @@ class LEMainWindow(QtWidgets.QMainWindow):
         self.populate_comboBox_selected_measuringpoint(measuringpoints=measuringpoint_list)
         # Удаление лишнего из списка некоммерческих присоединений
         self.non_profit_measuringpoints = set(non_profit_measuringpoints_list)
-        # Раскрыть корневой элемент в treeView
-        # self.ui.templateDataTree.setExpanded(self.template_data_model.index(0, 0), True)
         # Активировать пункт меню сохраняющий макет
         self.save_xml_action.setEnabled(True)
         # Активировать кнопку 'Применить'
@@ -276,7 +281,21 @@ class LEMainWindow(QtWidgets.QMainWindow):
             if period.attrib['end'] == end:
                 return measuringchannel
 
-    
+
+    def adjustment_volume(self, measuringpoint, measuringchannels_value, start, end):
+        '''
+        '''
+        root = self.tree.getroot()
+        for child in root.iterfind('.//'):
+            if (child.tag == 'measuringpoint') and (child.attrib['name'] == measuringpoint):
+                print(child.attrib['name'])
+                # for i in measuringchannels_value:
+                #     print([measuringchannel for measuringchannel in child if measuringchannel.attrib['code'] == i])
+
+
+
+
+
     def clicked_pushbutton_apply(self):
         '''
         Действия по нажатию кнопки "Применить".
@@ -287,26 +306,40 @@ class LEMainWindow(QtWidgets.QMainWindow):
         start = "".join(self.ui.startPeriod_comboBox.currentText().split(':'))
         end = "".join(self.ui.endPeriod_comboBox.currentText().split(':'))
         flag = int(self.ui.comboBox_select_flag.currentText())
+        measuringpoint = self.ui.comboBox_selected_measuringpoint.currentText()
         root = self.tree.getroot()
-        # Если присоединение было выбрано в TreeView
-        if self.ui.comboBox_measuringpoint_type.currentIndex() == 0:
-            for child in root.iterfind('.//'):
-                if (child.tag == 'measuringpoint') and (child.attrib['name'] == self.ui.comboBox_selected_measuringpoint.currentText()):
-                    for measuringchannel in child:
-                        measuringchannel = self.change_status(measuringchannel, start, end, flag)
-        # Для всех некоммерческих значений
-        elif self.ui.comboBox_measuringpoint_type.currentIndex() == 1:
-            for measuringpoint_in in self.non_profit_measuringpoints:
-                for measuringpoint in root.findall("./area/measuringpoint"):
-                    if measuringpoint.attrib['name'] == measuringpoint_in:
-                        for measuringchannel in measuringpoint:
+        # Действия для вкладки "Статус"
+        if self.ui.tabWidget.currentIndex() == 0:
+            # Если присоединение было выбрано вручную 
+            if self.ui.comboBox_measuringpoint_type.currentIndex() == 0:
+                for child in root.iterfind('.//'):
+                    if (child.tag == 'measuringpoint') and (child.attrib['name'] == self.ui.comboBox_selected_measuringpoint.currentText()):
+                        for measuringchannel in child:
                             measuringchannel = self.change_status(measuringchannel, start, end, flag)
-        # Для всего шаблона
-        elif self.ui.comboBox_measuringpoint_type.currentIndex() == 2:
-            for child in root.iterfind('.//'):
-                if (child.tag == 'measuringpoint'):
-                    for measuringchannel in child:
-                        measuringchannel = self.change_status(measuringchannel, start, end, flag)
+            # Для всех некоммерческих значений
+            elif self.ui.comboBox_measuringpoint_type.currentIndex() == 1:
+                for measuringpoint_in in self.non_profit_measuringpoints:
+                    for measuringpoint in root.findall("./area/measuringpoint"):
+                        if measuringpoint.attrib['name'] == measuringpoint_in:
+                            for measuringchannel in measuringpoint:
+                                measuringchannel = self.change_status(measuringchannel, start, end, flag)
+            # Для всего шаблона
+            elif self.ui.comboBox_measuringpoint_type.currentIndex() == 2:
+                for child in root.iterfind('.//'):
+                    if (child.tag == 'measuringpoint'):
+                        for measuringchannel in child:
+                            measuringchannel = self.change_status(measuringchannel, start, end, flag)
+        # Действия для вкладки "Объем"
+        elif self.ui.tabWidget.currentIndex() == 1:
+            measuringchannels_value = {
+                "01":(self.ui.lineEdit_a_plus.text(), self.ui.checkBox_save_a_plus.isChecked()),
+                "02":(self.ui.lineEdit_a_minus.text(), self.ui.checkBox_save_a_minus.isChecked()),
+                "03":(self.ui.lineEdit_r_plus.text(), self.ui.checkBox_save_r_plus.isChecked()),
+                "04":(self.ui.lineEdit_r_minus.text(), self.ui.checkBox_save_r_minus.isChecked())
+            }
+            self.adjustment_volume(measuringpoint, measuringchannels_value, start, end)
+        
+        # Перезагрузить treeview
         self.xml_to_treeview(root)
 
 
